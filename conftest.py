de0e2d23c1b37865b806d9946f325ee4d98997d4
Adams Tower <adams.tower@tartansolutions.com>
2020-10-29 16:56:08 -0400

    Missed one

--- conftest.py
+++ conftest.py
@@ -4,4 +4,4 @@ import platform
 collect_ignore = ["setup.py"]
 
 if platform.system() != "Windows":
-    collect_ignore.append("plaidtools/pcm_connection.py")
+    collect_ignore.append("plaidcloud/rpc/pcm_connection.py")
